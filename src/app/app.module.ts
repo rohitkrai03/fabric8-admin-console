7d01e06017ebb90b0afc468f57936b080fed5d1d
Rohit Rai <rohitkrai03@gmail.com>
2018-10-04 03:53:09 +0530

    feat(auth): register auth interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AUTH_API_URL, AuthenticationService, SSO_API_URL, REALM, WIT_API_PROXY } from 'ngx-login-client';
+import { AUTH_API_URL, AuthenticationService, SSO_API_URL, REALM, WIT_API_PROXY, AuthInterceptor } from 'ngx-login-client';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { Broadcaster } from 'ngx-base';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { PageNotFoundComponent } from './components/layout/page-not-found/page-not-found.component';
@@ -27,6 +27,7 @@ import { HeaderComponent } from './components/layout/header/header.component';
   providers: [
     AuthenticationService,
     Broadcaster,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     { provide: AUTH_API_URL, useValue: 'https://auth.prod-preview.openshift.io/api/' },
     { provide: SSO_API_URL, useValue: 'https://sso.prod-preview.openshift.io/api/' },
     { provide: WIT_API_PROXY, useValue: 'https://prod-preview.openshift.io/api/' },
